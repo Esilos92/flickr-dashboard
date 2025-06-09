d6a937eba0af0af6a6fc292e291aa83d2af1da1f
Esilos92 <vendette@galaxycon.com>
2025-06-09 06:54:53 -0400

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://68.183.149.181:3001';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://tip-olympus-dress-management.trycloudflare.com';
 
 export interface WatcherStatus {
   currentMode: 'ACTIVE' | 'SLEEP' | 'HIBERNATE';
